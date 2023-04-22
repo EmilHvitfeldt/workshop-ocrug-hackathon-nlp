9b3372861e6006cd76860eeb84bb9cd069d712c2
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-04-21 20:36:52 -0700

    rename data in grants

--- data-raw/grants.R
+++ data-raw/grants.R
@@ -9,7 +9,7 @@ grants <- grants_raw |>
   clean_names() |>
   filter(call_decision_year %in% c(2012, 2022)) |>
   filter(detect_language(abstract) == "en") |>
-  select(abstract, amount_granted_all_sets, call_decision_year, grant_number) |>
+  select(abstract, amount = amount_granted_all_sets, decision_year = call_decision_year, grant_number) |>
   drop_na()
 
 write_csv(grants, "data/grants.csv.gz")
